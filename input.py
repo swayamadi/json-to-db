a3772ae4f873e246c01bb64de5443ed470decca2
Aditya Verma <59619772+swayamadi@users.noreply.github.com>
2020-01-21 21:48:15 +0530

    Create input.py
    Sample Input

---
+++ input.py
@@ -0,0 +1,22 @@
+[
+  {
+    "author": "a", 
+    "name": "a", 
+    "published": "2019-05-01"
+  }, 
+  {
+    "author": "b", 
+    "name": "b", 
+    "published": "2019-05-27"
+  }, 
+  {
+    "author": "John Green", 
+    "name": "Looking for Alaska", 
+    "published": "2019-05-06"
+  }, 
+  {
+    "author": "ankthony doerr", 
+    "name": "all the light we cannot see", 
+    "published": "2019-04-04"
+  }
+]
