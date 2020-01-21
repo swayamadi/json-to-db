df983fed8ce808ccfa2f5757edefff9e2043d933
Aditya Verma <59619772+swayamadi@users.noreply.github.com>
2020-01-21 21:18:45 +0530

    uploaded imports.py

---
+++ imports.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("pip install flask")
+os.system("pip install requests")
+os.system("pip install jsonify")
+os.system("pip install flask_sqlalchemy")
+os.system("pip install flask_script")
+os.system("pip install flask_migrate")
+os.system("pip install psycopg2")
+os.system("pip install psycopg2-binary")
